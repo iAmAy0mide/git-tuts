0d85be8ea49d43d74897eed3bdd566a7f4306bc5
Ayomide <ayomidekenny952@gmail.com>
2023-12-27 23:25:25 -0800

    Version 1 upt

--- config.js
+++ config.js
@@ -1,5 +1,2 @@
 console.log('Version 2');
 console.log('Version 2 updated');
-if ('Version 2 is updated') {
-    console.log('We updated it!');
-}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 console.log('Version 2');
 console.log('Version 2');
-function tell() {
-    console.log('Updated');
-}
